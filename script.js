349ad746419465ddd7157f31a8c2d4dc5badc6df
Bernard Geric <r85b16gk@students.codeinstitute.net>
2024-11-04 21:59:11 +0000

    function set status

--- script.js
+++ script.js
@@ -101,3 +101,14 @@ function showQuestion(question) {
       scoreElement.innerHTML = `Final Score: ${score}/${questions.length}`;
     }
   }
+
+  function setStatusClass(element, correct) {
+    clearStatusClass(element);
+    if (correct) {
+      element.classList.add('correct');
+    } else {
+      element.classList.add('wrong');
+    }
+  } 
+
+  
