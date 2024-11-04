2b58a1d88efcb62db4260da959d45eb0f3e2c81e
Bernard Geric <r85b16gk@students.codeinstitute.net>
2024-11-04 21:53:31 +0000

    added questions in javascript

--- script.js
+++ script.js
@@ -9,3 +9,33 @@ const scoreValue = document.getElementById('score-value');
 
 let shuffledQuestions, currentQuestionIndex;
 let score = 0;
+
+const questions = [
+    {
+      question: "What is the capital of France?",
+      answers: [
+        { text: "Berlin", correct: false },
+        { text: "Madrid", correct: false },
+        { text: "Paris", correct: true },
+        { text: "Lisbon", correct: false }
+      ]
+    },
+    {
+      question: "Which language runs in a web browser?",
+      answers: [
+        { text: "Java", correct: false },
+        { text: "C", correct: false },
+        { text: "Python", correct: false },
+        { text: "JavaScript", correct: true }
+      ]
+    },
+    {
+      question: "What does CSS stand for?",
+      answers: [
+        { text: "Central Style Sheets", correct: false },
+        { text: "Cascading Style Sheets", correct: true },
+        { text: "Cascading Simple Sheets", correct: false },
+        { text: "Cars SUVs Sailboats", correct: false }
+      ]
+    }
+  ];
